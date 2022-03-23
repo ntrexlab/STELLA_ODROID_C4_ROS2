ddd40860025bf16a85bbadf4ca3576a448072975
ntrexlab <85467544+ntrexlab@users.noreply.github.com>
2022-03-23 10:55:09 +0900

    Add files via upload

---
+++ stella/stella_ahrs/src/MwAHRS.hpp
@@ -0,0 +1,8 @@
+#define ACC 0x33
+#define GYO 0x34
+#define DEG 0x35
+
+int16_t acc_x = 0, acc_y = 0, acc_z = 0, gyo_x = 0, gyo_y = 0, gyo_z = 0, deg_x = 0, deg_y = 0, deg_z = 0;
+int16_t _T = 1, _M = 13;
+
+bool Mw_AHRS_init(int ID);
